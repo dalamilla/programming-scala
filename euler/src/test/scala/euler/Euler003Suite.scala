64ad84f88ce0d45a7973e172444a54334ebd92bb
Daniel Alamilla <daniel.alamilla.co@gmail.com>
2025-03-19 21:11:20 +0000

    update to java 21 and fix errors on scalafmt

--- euler/src/test/scala/euler/Euler003Suite.scala
+++ euler/src/test/scala/euler/Euler003Suite.scala
@@ -2,10 +2,18 @@ package euler
 
 class Euler003Suite extends munit.FunSuite {
 
-  val parameters: List[(Long,Long)] =
-    List((2, 2), (3, 3), (5, 5), (7, 7), (8, 2), (13195, 29), (600851475143L, 6857))
+  val parameters: List[(Long, Long)] =
+    List(
+      (2, 2),
+      (3, 3),
+      (5, 5),
+      (7, 7),
+      (8, 2),
+      (13195, 29),
+      (600851475143L, 6857)
+    )
 
-  parameters.foreach{i =>
+  parameters.foreach { i =>
     test(s"The largest prime factor of ${i._1}: ${i._2}") {
       assertEquals(Euler003.solution(i._1), i._2)
     }
